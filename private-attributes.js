e23e98a951874cff13e400a419e8f6a15397d37a
Ben Nolan <bnolan@gmail.com>
2016-07-24 14:15:48 +1200

    Split into files.

---
+++ private-attributes.js
@@ -0,0 +1,25 @@
+var privateAttributes = new WeakMap();
+
+function setPrivateAttribute (element, name, value) {
+  if (!privateAttributes[element]) {
+    privateAttributes[element] = {};
+  }
+
+  privateAttributes[element][name] = value;
+}
+
+function getPrivateAttribute (element, name) {
+  return privateAttributes[element] && privateAttributes[element][name];
+}
+
+function removePrivateAttribute (element, name) {
+  if (privateAttributes[element]) {
+    delete privateAttributes[element][name];
+  }
+}
+
+module.exports = {
+  get: getPrivateAttribute,
+  set: setPrivateAttribute,
+  remove: removePrivateAttribute
+};
